6188833c7907696fb6648498c452dd61912a9327
crhino <piraino.chris@gmail.com>
2014-03-14 01:58:38 -0400

    Made Graph trait polymorphic.
    I used a collections::hashmap::HashMap to store
    vertices of type T and map them to uint for the
    matrix.

--- graph.rs
+++ graph.rs
@@ -6,17 +6,21 @@
  *  structures.
  *
  */
+extern crate collections;
+
 use std::vec;
 use std::result::Result;
+use collections::hashmap::HashMap;
+use std::hash;
 
-trait Graph {
-    fn new(vertices: uint) -> ~Self;
-    fn adjacent(&self, x: uint, y: uint) -> Result<bool, ~str>;
-    fn neighbors(&self, x: uint) -> ~[uint];
-    fn add(&mut self, x: uint, y: uint, val: int) -> Result<int, ~str>;
-    fn delete(&mut self, x: uint, y: uint) -> Result<int, ~str>;
-    fn get_edge_value(&self, x: uint, y: uint) -> Result<int, ~str>;
-    fn set_edge_value(&mut self, x: uint, y: uint, val: int) -> Result<int, ~str>;
+trait Graph<T> {
+    fn new(vert: uint) -> ~Self;
+    fn adjacent(&self, x: &T, y: &T) -> Result<bool, ~str>;
+    fn neighbors<'a>(&'a self, x: &T) -> Result<~[&'a T], ~str>;
+    fn add(&mut self, x: T, y: T, val: int) -> Result<int, ~str>;
+    fn delete(&mut self, x: &T, y: &T) -> Result<int, ~str>;
+    fn get_edge_value(&self, x: &T, y: &T) -> Result<int, ~str>;
+    fn set_edge_value(&mut self, x: &T, y: &T, val: int) -> Result<int, ~str>;
 }
 
 trait Matrix {
@@ -61,19 +65,55 @@ impl Matrix for VectorMatrix {
     fn width(&self) -> uint {self.width}
 }
 
-impl Graph for VectorMatrix {
-    fn new(vertices: uint) -> ~VectorMatrix {
-        Matrix::zero(vertices, vertices)
+struct AdjacencyMatrixGraph<T> {
+    matrix: ~VectorMatrix,
+    vertices: HashMap<T, uint>,
+    size: uint
+}
+
+/*
+impl<T: hash::Hash + Eq + std::fmt::Show> AdjacencyMatrixGraph<T> {
+    fn vertex_to_uint(&self, x: &T) -> Result<uint, ~str> {
+        let opt_u = self.vertices.find(x);
+        if opt_u.is_none() {    
+            Err(format!("Vertex ({}) was not found in the graph", x))
+        } else {
+            Ok(*(opt_u.unwrap()))
+        }
     }
-    fn adjacent(&self, x: uint, y: uint) -> Result<bool, ~str> {
-        let res = self.at(x, y);
+}
+*/
+
+impl<T: hash::Hash + Eq + std::fmt::Show> Graph<T> for AdjacencyMatrixGraph<T> {
+    fn new(vert: uint) -> ~AdjacencyMatrixGraph<T> {
+        let m: ~VectorMatrix = Matrix::zero(vert, vert);
+        let v = HashMap::<T, uint>::with_capacity(vert);
+        ~AdjacencyMatrixGraph {matrix: m, vertices: v, size: vert}
+    }
+    fn adjacent(&self, x: &T, y: &T) -> Result<bool, ~str> {
+        let opt_u = self.vertices.find(x);
+        let opt_w = self.vertices.find(y);
+        match (opt_u, opt_w) {
+            (None, _) => return Err(format!("Vertex x: ({}) was not found in the graph", x)),
+            (_, None) => return Err(format!("Vertex y: ({}) was not found in the graph", y)),
+            (_, _) => { }
+        }
+        let res = self.matrix.at(*(opt_u.unwrap()), *(opt_w.unwrap()));
         match res {
             Ok(n) => if n == 0 { Ok(false) } else { Ok(true) },
             Err(err) => Err(err)
         }
     }
-    fn neighbors(&self, x: uint) -> ~[uint] {
-        let row = self.elements.slice(x*self.width, x*self.width+self.width);
+    fn neighbors<'a>(&'a self, x: &T) -> Result<~[&'a T], ~str> {
+        let opt_u = self.vertices.find(x);
+        if opt_u.is_none() {
+            return Err(format!("Vertex x: ({}) was not found in the graph", x));
+        }
+        let u = *(opt_u.unwrap());
+        let row = self.matrix.elements.slice(u*self.matrix.width, 
+                                             (u+1)*self.matrix.width);
+        // Builds vector of uints, which represent vertices that are
+        // adjacent to vertex x.
         let build = |push: |v: uint|| {
             for r in row.iter().enumerate() {
                 let (i, val) = r;
@@ -82,20 +122,56 @@ impl Graph for VectorMatrix {
                 }
             }
         };
-        vec::build(None, build)
+        let verts_uint = vec::build(None, build);
+        // Filter_map on vertices' Entries to build a vector ~[&'a T].
+        // If the value exists in the verts_uint vector, that key is a
+        // neighbor and thus is added to the ~[&'a T].
+        Ok(self.vertices.iter().filter_map(|(k, v)| {
+            for i in verts_uint.iter() {
+                if i == v {
+                    return Some(k);
+                }
+            }
+            None
+        }).to_owned_vec())
     }
-    fn add(&mut self, x: uint, y: uint, val: int) -> Result<int, ~str> {
-        self.set(x, y, val)
+    fn add(&mut self, x: T, y: T, val: int) -> Result<int, ~str> {
+        let len = self.vertices.len(); 
+        let x_val = *self.vertices.find_or_insert(x, len);
+        let len = self.vertices.len(); 
+        let y_val = *self.vertices.find_or_insert(y, len);
+        self.matrix.set(x_val, y_val, val)
     }
-    fn delete(&mut self, x: uint, y: uint) -> Result<int, ~str> {
-        self.set(x, y, 0)
+    fn delete(&mut self, x: &T, y: &T) -> Result<int, ~str> {
+        let opt_u = self.vertices.find(x);
+        let opt_w = self.vertices.find(y);
+        match (opt_u, opt_w) {
+            (None, _) => return Err(format!("Vertex x: ({}) was not found in the graph", x)),
+            (_, None) => return Err(format!("Vertex y: ({}) was not found in the graph", y)),
+            (_, _) => { }
+        }
+        self.matrix.set(*(opt_u.unwrap()), *(opt_w.unwrap()),0)
     }
-    fn get_edge_value(&self, x: uint, y: uint) -> Result<int, ~str> {
-        self.at(x, y)
+    fn get_edge_value(&self, x: &T, y: &T) -> Result<int, ~str> {
+        let opt_u = self.vertices.find(x);
+        let opt_w = self.vertices.find(y);
+        match (opt_u, opt_w) {
+            (None, _) => return Err(format!("Vertex x: ({}) was not found in the graph", x)),
+            (_, None) => return Err(format!("Vertex y: ({}) was not found in the graph", y)),
+            (_, _) => { }
+        }
+        self.matrix.at(*(opt_u.unwrap()), *(opt_w.unwrap()))
     }
-    fn set_edge_value(&mut self, x: uint, y: uint, val: int) -> Result<int, ~str> {
+    fn set_edge_value(&mut self, x: &T, y: &T, val: int) -> Result<int, ~str> {
         // TODO: If x, y are not adjacent, return Err().
-        self.set(x, y, val)
+        let opt_u = self.vertices.find(x);
+        let opt_w = self.vertices.find(y);
+        match (opt_u, opt_w) {
+            (None, _) => return Err(format!("Vertex x: ({}) was not found in the graph", x)),
+            (_, None) => return Err(format!("Vertex y: ({}) was not found in the graph", y)),
+            (_, _) => { }
+        }
+        self.matrix.set(*(opt_u.unwrap()), *(opt_w.unwrap()), val)
     }
 }
 
@@ -125,26 +201,28 @@ fn test_matrix_set_bounds() {
 
 #[test]
 fn test_graph_neighbors() {
-    let mut graph: ~VectorMatrix = Graph::new(3);
-    assert!(!graph.adjacent(1, 2).ok().unwrap());
+    let mut graph: ~AdjacencyMatrixGraph<int> = Graph::new(3);
     let mut res = graph.add(0,1,1);
     assert!(res.is_ok());
     res = graph.add(0,2,1);
     assert!(res.is_ok());
-    let neighbors = graph.neighbors(0);
-    assert_eq!(neighbors.len(), 2);
-    assert_eq!(neighbors[0], 1);
-    assert_eq!(neighbors[1], 2);
+    let neighbors = graph.neighbors(&0);
+    assert!(neighbors.is_ok());
+    let neigh = neighbors.unwrap();
+    println!("{}", neigh);
+    assert_eq!(neigh.len(), 2);
+    assert_eq!(*neigh[0], 1);
+    assert_eq!(*neigh[1], 2);
 }
 
 #[test]
 fn test_graph_adjacent() {
-    let mut graph: ~VectorMatrix = Graph::new(3);
-    assert!(!graph.adjacent(1, 2).ok().unwrap());
+    let mut graph: ~AdjacencyMatrixGraph<int> = Graph::new(3);
+    assert!(graph.adjacent(&1, &2).is_err());
     let mut res = graph.add(0,1,1);
     assert!(res.is_ok());
     res = graph.add(0,2,1);
     assert!(res.is_ok());
-    assert!(graph.adjacent(0,1).ok().unwrap());
-    assert!(graph.adjacent(0,2).ok().unwrap());
+    assert!(graph.adjacent(&0,&1).unwrap());
+    assert!(graph.adjacent(&0,&2).unwrap());
 }
